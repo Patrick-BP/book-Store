5cea9bd6c761365b7a8600110115d1a28d8679f7
patrick-bp <papybp@gmail.com>
2025-01-27 22:56:20 -0700

    kghkjk

--- backend/src/middleware/verifyAdminToken.js
+++ backend/src/middleware/verifyAdminToken.js
@@ -6,10 +6,10 @@ const verifyAdminToken  =(req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1];
     if(!token) return res.status(401).send({message: "Access denied!"})
 
-    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    jwt.verify(token, JWT_SECRET, (err, user) => {
         if(err) return res.status(403).send({message: "Invalid token!"})
-        if(decoded.role !== 'admin') return res.status(401).send({message: "Unauthorized!"})
-        req.user = decoded;
+
+        req.user = user;
         next();
     })
 }
